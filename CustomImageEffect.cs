893694f65fa4f4411140dcd3072913a83cbfa4fc
Jayden Lee <34024244+Shroomy1@users.noreply.github.com>
2019-02-19 10:29:52 +1100

    more stuff

---
+++ CustomImageEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomImageEffect : MonoBehaviour {
+
+	public Material material;
+
+	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+		Graphics.Blit(src, dest, material);
+	}
+}
